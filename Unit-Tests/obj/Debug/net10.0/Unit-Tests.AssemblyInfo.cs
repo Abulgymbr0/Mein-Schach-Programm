e7a6f761804c1f08d906c53f351e6b37c8cfc6fa
Student <foobar@sr.htlweiz.at>
2026-04-13 11:16:35 +0200

    finished 3

--- Unit-Tests/obj/Debug/net10.0/Unit-Tests.AssemblyInfo.cs
+++ Unit-Tests/obj/Debug/net10.0/Unit-Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit-Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4220c7e61815a9b5d6b2b06f87d4471eda4ead74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6859cb16b2c4b4ba5fd5fabc1ebef36355e09e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit-Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit-Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
