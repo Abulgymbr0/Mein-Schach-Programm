0d6859cb16b2c4b4ba5fd5fabc1ebef36355e09e
Student <foobar@sr.htlweiz.at>
2026-04-13 11:14:19 +0200

    finished 2

--- Bibliotek/Class1.cs
+++ Bibliotek/Class1.cs
@@ -2,5 +2,12 @@
 
 public class Class1
 {
-
+public override string ToString()
+    {
+        var sb = new System.Text.StringBuilder();
+        int size = 8;
+ 
+        string line = "+";
+        for (int i = 0; i < size; i++)
+        line += "--+";
 }
--- Unit-Tests/obj/Debug/net10.0/Unit-Tests.AssemblyInfo.cs
+++ Unit-Tests/obj/Debug/net10.0/Unit-Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit-Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2f408d062c4abef37fe584e75d907d2f21eb4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4220c7e61815a9b5d6b2b06f87d4471eda4ead74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit-Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit-Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
